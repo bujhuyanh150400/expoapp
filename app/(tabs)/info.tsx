b14c68d2382ed2de53e9d3058ac32f7a7f4e0c73
Huy Anh Bùi <anh.bh@hostingviet.vn>
2025-04-29 17:24:36 +0700

    AnhBH - init project

---
+++ app/(tabs)/info.tsx
@@ -0,0 +1,9 @@
+import {Text, View} from "react-native";
+
+export default function Info() {
+    return (
+        <View className="flex-1 justify-center items-center">
+            <Text className="text-5xl text-blue-400 font-bold">InfoScreen</Text>
+        </View>
+    );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,7 @@ import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-
+import '@/app/global.css';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -28,10 +28,9 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
